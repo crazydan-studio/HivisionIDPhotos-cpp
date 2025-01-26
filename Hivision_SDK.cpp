aefbb122f235bed7648ec89621b8db8050f5d482
flytreeleft <flytreeleft@crazydan.org>
2025-01-26 13:57:24 +0800

    结构化背景色数据，并支持指定输出图片的格式，同时将输出图片按目录组织

--- Hivision_SDK.cpp
+++ Hivision_SDK.cpp
@@ -7,15 +7,15 @@
 void human_mating (Hivision_config hivision){
 	std::string ImagePathStr(hivision.image_path);
 	std::string ModelPathStr(hivision.model_path);
-	std::string OutPathStr(hivision.out_path);
+	std::string OutPathStr(hivision.out_image_path);
 
     const char* ImagePath = ImagePathStr.c_str();
 	const char* ModelPath = ModelPathStr.c_str();
-	
+
 
 	cv::Mat image =cv::imread(ImagePath, cv::IMREAD_COLOR);
-    cv::Mat bgra_img=human_matting(ModelPath,image,4);
-    cv::imwrite(OutPathStr+"human_matting.png", bgra_img);
+    cv::Mat bgra_img=human_matting(ModelPath,image,hivision.thread_num);
+    cv::imwrite(OutPathStr+"/human_matting."+hivision.out_image_type, bgra_img);
 
 }
 
@@ -26,17 +26,17 @@ int ID_photo (Hivision_config hivision,int out_size_kb,bool layout_phot){
     matting_params human_matting_params;
 	std::string ImagePathStr(hivision.image_path);
 	std::string ModelPathStr(hivision.model_path);
-	std::string OutPathStr(hivision.out_path);
+	std::string OutPathStr(hivision.out_image_path);
 	std::string FaceModelPathStr(hivision.face_model_path);
 
     const char* ImagePath = ImagePathStr.c_str();
 	const char* ModelPath = ModelPathStr.c_str();
 
-	cv::Vec3b newBackgroundColor(hivision.background_b,hivision.background_g,hivision.background_r);
+	cv::Vec3b newBackgroundColor(hivision.background_color.b,hivision.background_color.g,hivision.background_color.r);
     LFFD* face_detector = new LFFD(FaceModelPathStr,hivision.model_scale ,hivision.thread_num );
 	cv::Mat image =cv::imread(ImagePath, cv::IMREAD_COLOR);
 
-	cv::Mat bgra_img=human_matting(ModelPath, image,4);
+	cv::Mat bgra_img=human_matting(ModelPath, image,hivision.thread_num);
     cv::Mat add_background_img = addBackground(bgra_img, newBackgroundColor);
 	cv::cvtColor(add_background_img, add_background_img, cv::COLOR_BGRA2BGR);
 
@@ -55,16 +55,16 @@ int ID_photo (Hivision_config hivision,int out_size_kb,bool layout_phot){
 		human_matting_params.face_info = finalBox[0];
 	}
 	free(face_detector);
-    cv::Mat hd_result = photo_adjust(human_matting_params, add_background_img, hivision.out_image_height,hivision.out_image_width,hivision.background_r,hivision.background_g,hivision.background_b, hivision.head_measure_ratio);
+    cv::Mat hd_result = photo_adjust(human_matting_params, add_background_img, hivision.out_image_height,hivision.out_image_width,hivision.background_color.r,hivision.background_color.g,hivision.background_color.b, hivision.head_measure_ratio);
     cv::Mat standard_result;
 	cv::Size standard_size(hivision.out_image_width,hivision.out_image_height);
     cv::resize(hd_result, standard_result, standard_size);
 
 	if(out_size_kb>0){
-    
-		resizeImageToKB(hd_result,OutPathStr+"result_kb.png",out_size_kb);
+
+		resizeImageToKB(hd_result,OutPathStr+"/result_kb."+hivision.out_image_type,out_size_kb);
 	}
-	
+
     if(layout_phot){
 		auto result_typography_arr =generate_layout_photo(hivision.out_image_height,hivision.out_image_width);
 		cv::Mat result_layout_image = generate_layout_image(
@@ -74,10 +74,10 @@ int ID_photo (Hivision_config hivision,int out_size_kb,bool layout_phot){
 		hivision.out_image_width,
 		hivision.out_image_height
 		);
-		cv::imwrite( OutPathStr+"layout_photo.png", result_layout_image);
+		cv::imwrite( OutPathStr+"/layout_photo."+hivision.out_image_type, result_layout_image);
 	 }
-	cv::imwrite( OutPathStr+"result_hd.png", hd_result);
-	cv::imwrite(OutPathStr+"result_standard.png", standard_result);
+	cv::imwrite( OutPathStr+"/result_hd."+hivision.out_image_type, hd_result);
+	cv::imwrite(OutPathStr+"/result_standard."+hivision.out_image_type, standard_result);
 	return 1;
-   
+
 }
--- include/HIvision_SDK.h
+++ include/HIvision_SDK.h
@@ -10,23 +10,33 @@
 #endif
 
 
+extern "C" DLL_API typedef struct Hivision_color
+
+{
+
+    int r;
+    int g;
+    int b;
+
+}Hivision_color;
+
+
 extern "C" DLL_API typedef struct Hivision_config
 
 {
 
     char* model_path;
-    char* image_path;
-    char* out_path;
     char* face_model_path;
-    int background_r=255;
-    int background_g=0;
-    int background_b=0;
+    char* image_path;
+    char* out_image_path;
+    char* out_image_type;
+    Hivision_color background_color=Hivision_color{.r=255,.g=0,.b=0};
     int thread_num=4;
     int model_scale=8;
     float head_measure_ratio = 0.35;
     int out_image_width = 295;
 	int out_image_height = 413;
-    
+
 
 }Hivision_config;
 extern "C" DLL_API int ID_photo (Hivision_config hivision,int out_size_kb=-1,bool layout_phot=false);
